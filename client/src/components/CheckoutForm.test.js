b074a967f52aa437410c3f7705f04257657a23a7
meep-morp <gracieaerogirl@gmail.com>
2020-06-06 09:47:32 -0500

    finished MVP

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+	render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+	const header = screen.getByText(/checkout form/i);
+	expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+	render(<CheckoutForm />);
+
+	const firstName = screen.getByLabelText(/first Name/i);
+	const lastName = screen.getByLabelText(/last Name/i);
+	const address = screen.getByLabelText(/address/i);
+	const city = screen.getByLabelText(/city/i);
+	const state = screen.getByLabelText(/state/i);
+	const zip = screen.getByLabelText(/zip/i);
+
+	fireEvent.change(firstName, { target: { value: "Adrian" } });
+	fireEvent.change(lastName, { target: { value: "Hartley" } });
+	fireEvent.change(address, { target: { value: "!0 West Johnson Street" } });
+	fireEvent.change(city, { target: { value: "Jasper" } });
+	fireEvent.change(state, { target: { value: "Al" } });
+    fireEvent.change(zip, { target: { value: "35503" } });
+    
+    const checkoutArr = screen.getAllByText(/checkout/i);
+    const checkoutButton = checkoutArr[1];
+    fireEvent.click(checkoutButton);
+
+    const successMessage = screen.getByTestId("successMessage");
+    expect(successMessage).toBeInTheDocument;
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,19 @@
+import react, { useState } from "react";
+
 // write your custom hook here to control your checkout form
+
+export const useForm = initialValue => {
+	const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+	const [values, setValues] = useState(initialValue);
+
+	const handleChanges = e => {
+		setValues({ ...values, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		setShowSuccessMessage(true);
+	};
+
+	return [handleChanges, handleSubmit, values, showSuccessMessage];
+};
